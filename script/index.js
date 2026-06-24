2e2dd4f46a351887cf0a9e383f64885ba6d111c7
Alex Kizenkov <aliaksandr.kiziankou@gmail.com>
2026-06-24 18:41:28 +0500

    feat: add burger menu functionality

--- script/index.js
+++ script/index.js
@@ -1,14 +1,16 @@
-console.log(
-    'Self-assessment:\n'+
-    'Main page:\n'+
-    'Markup validation +10\n'+
-    'Layout matches the design +35\n'+
-    'CSS requirements +15\n'+
-    'Interactivity +10\n'+
-    'Pets page:\n'+
-    'Markup validation +10\n'+
-    'Layout matches the design +15\n'+
-    'CSS requirements +5\n'+
-    'Interactivity +10\n'+
-    'Total: 110'
-);
+const BURGER_BTN = document.querySelector('.burger-btn');
+const BURGER_MENU = document.querySelector('.burger-menu');
+
+BURGER_BTN.addEventListener('click', () => {
+    BURGER_MENU.classList.toggle('burger_open');
+    BURGER_BTN.classList.toggle('rotate');
+    document.body.classList.toggle('lock');
+});
+
+BURGER_MENU.addEventListener('click', (event) => {
+    if (event.target.classList.contains('burger-menu') || event.target.classList.contains('burger-link')) {
+        BURGER_MENU.classList.remove('burger_open');
+        document.body.classList.toggle('lock');
+        BURGER_BTN.classList.remove('rotate');
+    }
+});
