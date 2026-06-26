cc71f8633fcd0596d4490a571b413a4ddee35c69
Alex Kizenkov <aliaksandr.kiziankou@gmail.com>
2026-06-26 19:13:54 +0500

    feat: add popup

--- script/index.js
+++ script/index.js
@@ -32,7 +32,7 @@ let ALL_CARDS = [];
 let threeCards = [];
 let remainingCards = [];
 
-async function init() {
+async function sliderCards() {
     const RESPONSE = await fetch('script/pets.json');
     ALL_CARDS = await RESPONSE.json();
     threeCards = shuffleCards([...ALL_CARDS]).slice(0, 3);
@@ -40,13 +40,13 @@ async function init() {
     renderCards(threeCards);
 }
 
-init();
+sliderCards();
 
 const SLIDER = document.querySelector('.slider__row');
 
 function renderCards(ALL_CARDS) {
     SLIDER.innerHTML = ALL_CARDS.map(card => `
-        <div class="pets__card">
+        <div class="pets__card" data-name="${card.name}">
             <div class="card__img">
                 <img src="${card.img}" alt="${card.name}">
             </div>
@@ -68,9 +68,7 @@ function shuffleCards(arr) {
 
 const BTN_PREV = document.querySelector('.arrow.prev');
 const BTN_NEXT = document.querySelector('.arrow.next');
-
-BTN_PREV.addEventListener('click', switchCards);
-BTN_NEXT.addEventListener('click', switchCards);
+const BTN_PREV_MOBILE = document.querySelector('.arrow.prev.mobile');
 
 function switchCards() {
     const newThreeCards = remainingCards.slice(0, 3);
@@ -78,3 +76,72 @@ function switchCards() {
     threeCards = newThreeCards;
     renderCards(threeCards);
 }
+
+BTN_PREV.addEventListener('click', transitionLeft);
+BTN_NEXT.addEventListener('click', transitionRight);
+BTN_PREV_MOBILE.addEventListener('click', transitionLeft);
+
+function transitionLeft() {
+    SLIDER.classList.add('slide-left');
+    BTN_PREV.removeEventListener('click', transitionLeft);
+    BTN_PREV_MOBILE.removeEventListener('click', transitionLeft);
+}
+
+function transitionRight() {
+    SLIDER.classList.add('slide-right');
+    BTN_NEXT.removeEventListener('click', transitionRight);
+}
+
+SLIDER.addEventListener('transitionend', (event) => {
+    if (event.target !== SLIDER) return;
+    if (!SLIDER.classList.contains('slide-left') && !SLIDER.classList.contains('slide-right')) return;
+
+    SLIDER.style.transition = 'none';
+    SLIDER.classList.remove('slide-left');
+    SLIDER.classList.remove('slide-right');
+    switchCards();
+    SLIDER.getBoundingClientRect();
+    SLIDER.style.transition = '';
+    BTN_PREV.addEventListener('click', transitionLeft);
+    BTN_NEXT.addEventListener('click', transitionRight);
+    BTN_PREV_MOBILE.addEventListener('click', transitionLeft);
+});
+
+/*Popup*/
+
+const POPUP = document.querySelector('.pop-up');
+const MODAL_WINDOW = document.querySelector('.pop-up__content');
+
+SLIDER.addEventListener('click', (event) => {
+    const card = event.target.closest('.pets__card');
+    if (!card) return;
+
+    const cardName = card.dataset.name;
+    const currentCard = ALL_CARDS.find(obj => obj.name === cardName);
+
+    MODAL_WINDOW.innerHTML = `
+        <img src="${currentCard.img}" alt="${currentCard.name}">
+        <div class="pop-up__text">
+            <h3>${currentCard.name}</h3>
+            <h4>${currentCard.type} - ${currentCard.breed}</h4>
+            <h5>${currentCard.description}</h5>
+            <ul>
+                <li>Age: <span>${currentCard.age}</span></li>
+                <li>Inoculations: <span>${currentCard.inoculations}</span></li>
+                <li>Diseases: <span>${currentCard.diseases}</span></li>
+                <li>Parasites: <span>${currentCard.parasites}</span></li>
+            </ul>
+        </div>
+    `;
+
+    POPUP.classList.remove('hidden');
+    document.documentElement.classList.add('lock');
+    
+});
+
+POPUP.addEventListener('click', (event) => {
+    if (event.target.classList.contains('pop-up') || event.target.classList.contains('modal-btn')) {
+        POPUP.classList.add('hidden');
+        document.documentElement.classList.remove('lock');
+    }
+});
